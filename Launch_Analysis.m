d5c4626f82abe4bc96c58e68990d72f610892faa
drromano88 <drromano88@gmail.com>
2019-04-07 13:15:56 -0400

    Launch 2 Stuff
    Data Analysis for Launch 2

--- Launch_Analysis.m
+++ Launch_Analysis.m
@@ -1,9 +1,9 @@
 %Code for Analyzing Launch
 %clear; %clear variables
 clc; %clear command window
-
-load Launch_1.txt
-launch=Launch_1;
+clear all;
+load Launch_3.txt
+launch=Launch_3;
 x_accel=launch(:,1);
 y_accel=launch(:,2);
 z_accel=launch(:,3);
@@ -33,11 +33,33 @@ log_time=(0:0.02:o/50)';
 % hold off
 
 hold on
-plot(log_time(1:o,:),-z_roll/(2*pi))
-plot(time+(i/50),ohmega)
-legend('Measured Roll Rate','Simulated Roll Rate')
-xlabel('Time (s)')
-ylabel('Rocket Roll Rate (Hz)')
-title('Actual vs. Simulated Roll Rate')
-axis([i/50-1 i/50+100 -inf inf])
+%plot(log_time(1:o,:),-z_roll/(2*pi))
+rx = .01;
+ry = .001;
+ohmega_x = sqrt(abs(x_accel/rx))*(1/(2*pi));
+ohmega_y = sqrt(abs(y_accel/ry))*(1/(2*pi));
+plot(log_time(1:o,:), ohmega_x);
+plot(log_time(1:o,:), ohmega_y);
+axis([i/50 i/50+10 0 15])
+title('Estimated Roll Rate')
+legend('ohmega_X','ohmega_Y')
 hold off
+%figure
+%plot(log_time(1:o,:),x_accel)
+%x_accel_fft = y_accel(i:i+250);
+%P2 = abs(fft(x_accel_fft/250));
+%P1 = P2(1:26);
+%P1(2:end-1) = 2*P1(2:end-1);
+%Fs = 50;
+%f = Fs*(0:(50/2))/50;
+%plot(log_time(1:o,:),y_accel)
+%plot(log_time(1:o,:),z_accel)
+%plot(time+(i/50),ohmega)
+%legend('X_accel','Y_accel','Z_accel')
+%xlabel('Time (s)')
+%ylabel('Rocket Roll Rate (Hz)')
+%title('Actual vs. Simulated Roll Rate')
+%axis([i/50 i/50+5 -80 80])
+%hold off
+%figure
+%plot(f,P1);
