b3830094abf3d9d0b5670a2823d75cf5283d9f1a
drromano88 <drromano88@gmail.com>
2019-04-09 19:10:29 -0400

    Fixed the Moment Calculator
    Moment Calculator is all set for implementation into code, just comment and uncomment stuffs

--- Moment_Calculator.m
+++ Moment_Calculator.m
@@ -3,21 +3,22 @@
 %variables, comment out for actual code usage
 length = 6*0.0254; %chord 1
 width = 4*.0254;
-chordtip = 1 *.0254; %chord2
+chordtip = 6 *.0254; %chord2
 rho = 1.2;%air density
 V = 343/2;%m/s
 theta = 2*pi/180;
-ohmega = 5*2*pi; %radians per second
+Vy = sin(theta)*V;
+ohmega = 11*2*pi; %radians per second
 r =.0254; %rocket radius m
 stepsize = .001;
 finnum = 3;
 
 %%Magic Happens Here, Cool to compare to Cp estimates 
-%%ohmega is also a function of position, need to account for that
 intwidth = r:stepsize:width+r; %width at which to intergrate the moment over
-%%NEED TO CHANGE THE FOLLOWING LINE TO FIX EQUILIBRIUM
 vofr = intwidth*ohmega; %calculate anglar velocity across the fin
+calcvel = Vy - vofr; %oncoming fin velocity
+thetafull = atan(calcvel./V); %angle of attack for each fin portion
 lengthint = -((length-chordtip)/width)*(intwidth-r)+length; %calculate the length in 1mm imcrements
-cl = 2*pi*theta; %find the lift coefficient
-M = finnum*cl*rho*sum(vofr.^2.*lengthint.*stepsize) %find the moment based on the fins
+cl = 2*pi*thetafull; %find the lift coefficient
+M = 1/2*finnum*rho*sum(cl.*V^2.*lengthint.*stepsize.*intwidth) %find the moment based on the fins
 
